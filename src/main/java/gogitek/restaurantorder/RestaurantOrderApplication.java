ea580c9523e19b1724cf8621032298efc4f1351d
namchtq238 <kaizuto123@gmail.com>
2022-10-15 00:35:20 +0700

    initial commit

---
+++ src/main/java/gogitek/restaurantorder/RestaurantOrderApplication.java
@@ -0,0 +1,13 @@
+package gogitek.restaurantorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantOrderApplication.class, args);
+    }
+
+}
